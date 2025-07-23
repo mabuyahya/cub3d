dc73cb996cf664148a6be2ed868bf744e9ef30b1
mabuyahya <mabuyahya@student.42amman.com>
2025-07-23 17:52:28 +0300

    normenat half the ray_casting folder

--- srcs/ray_casting/controls.c
+++ srcs/ray_casting/controls.c
@@ -1,55 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   controls.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mabuyahy <mabuyahy@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/23 17:01:56 by mabuyahy          #+#    #+#             */
+/*   Updated: 2025/07/23 17:06:00 by mabuyahy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "cub3d.h"
 
-void move_forward(t_game *game)
+void	move_forward(t_game *game)
 {
-    double new_x;
-    double new_y;
+	double	new_x;
+	double	new_y;
 
-    new_x = game->player.position.x + game->player.direction.x * MOVE_SPEED;
-    new_y = game->player.position.y + game->player.direction.y * MOVE_SPEED;
-    if (game->scene->map->map_2d[(int)new_y][(int)game->player.position.x] != '1')
-    game->player.position.y = new_y;
-    if (game->scene->map->map_2d[(int)game->player.position.y][(int)new_x] != '1')
-    game->player.position.x = new_x;
+	new_x = game->player.position.x + game->player.direction.x * MOVE_SPEED;
+	new_y = game->player.position.y + game->player.direction.y * MOVE_SPEED;
+	if (game->scene->map->map_2d[(int)new_y][(int)game->player.position.x]
+		!= '1')
+		game->player.position.y = new_y;
+	if (game->scene->map->map_2d[(int)game->player.position.y][(int)new_x]
+		!= '1')
+		game->player.position.x = new_x;
 }
 
-void move_backward(t_game *game)
+void	move_backward(t_game *game)
 {
-    double new_x;
-    double new_y;
+	double	new_x;
+	double	new_y;
 
-    new_x = game->player.position.x - game->player.direction.x * MOVE_SPEED;
-    new_y = game->player.position.y - game->player.direction.y * MOVE_SPEED;
-    if (game->scene->map->map_2d[(int)new_y][(int)game->player.position.x] != '1')
-        game->player.position.y = new_y;
-    if (game->scene->map->map_2d[(int)game->player.position.y][(int)new_x] != '1')
-        game->player.position.x = new_x;
+	new_x = game->player.position.x - game->player.direction.x * MOVE_SPEED;
+	new_y = game->player.position.y - game->player.direction.y * MOVE_SPEED;
+	if (game->scene->map->map_2d[(int)new_y][(int)game->player.position.x]
+		!= '1')
+		game->player.position.y = new_y;
+	if (game->scene->map->map_2d[(int)game->player.position.y][(int)new_x]
+		!= '1')
+		game->player.position.x = new_x;
 }
 
-void move_left(t_game *game)
+void	move_left(t_game *game)
 {
-    double new_x;
-    double new_y;
+	double	new_x;
+	double	new_y;
 
-    new_x = game->player.position.x + game->player.direction.y * MOVE_SPEED;
-    new_y = game->player.position.y - game->player.direction.x * MOVE_SPEED;
-    if (game->scene->map->map_2d[(int)new_y][(int)game->player.position.x] != '1')
-        game->player.position.y = new_y;
-    if (game->scene->map->map_2d[(int)game->player.position.y][(int)new_x] != '1')
-        game->player.position.x = new_x;
+	new_x = game->player.position.x + game->player.direction.y * MOVE_SPEED;
+	new_y = game->player.position.y - game->player.direction.x * MOVE_SPEED;
+	if (game->scene->map->map_2d[(int)new_y][(int)game->player.position.x]
+		!= '1')
+		game->player.position.y = new_y;
+	if (game->scene->map->map_2d[(int)game->player.position.y][(int)new_x]
+		!= '1')
+		game->player.position.x = new_x;
 }
 
-void move_right(t_game *game)
+void	move_right(t_game *game)
 {
-    double new_x;
-    double new_y;
+	double	new_x;
+	double	new_y;
 
-    new_x = game->player.position.x - game->player.direction.y * MOVE_SPEED;
-    new_y = game->player.position.y + game->player.direction.x * MOVE_SPEED;
-    if (game->scene->map->map_2d[(int)new_y][(int)game->player.position.x] != '1')
-        game->player.position.y = new_y;
-    if (game->scene->map->map_2d[(int)game->player.position.y][(int)new_x] != '1')
-        game->player.position.x = new_x;
+	new_x = game->player.position.x - game->player.direction.y * MOVE_SPEED;
+	new_y = game->player.position.y + game->player.direction.x * MOVE_SPEED;
+	if (game->scene->map->map_2d[(int)new_y][(int)game->player.position.x]
+		!= '1')
+		game->player.position.y = new_y;
+	if (game->scene->map->map_2d[(int)game->player.position.y][(int)new_x]
+		!= '1')
+		game->player.position.x = new_x;
 }
-
-
--- srcs/ray_casting/game_setup.c
+++ srcs/ray_casting/game_setup.c
@@ -1,105 +1,119 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   game_setup.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mabuyahy <mabuyahy@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/23 17:02:56 by mabuyahy          #+#    #+#             */
+/*   Updated: 2025/07/23 17:19:42 by mabuyahy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "cub3d.h"
 
-t_point find_player_direction(char direction)
+t_point	find_player_direction(char direction)
 {
-    t_point dir;
-    
-    dir.x = 0;
-    dir.y = 0;
-    if (direction == 'N')
-    {
-        dir.x = 0;
-        dir.y = -1;
-    }
-    else if (direction == 'S')
-    {
-        dir.x = 0;
-        dir.y = 1;
-    }
-    else if (direction == 'E')
-    {
-        dir.x = 1;
-        dir.y = 0;
-    }
-    else if (direction == 'W')
-    {
-        dir.x = -1;
-        dir.y = 0;
-    }
-    return (dir);
+	t_point	dir;
+
+	dir.x = 0;
+	dir.y = 0;
+	if (direction == 'N')
+	{
+		dir.x = 0;
+		dir.y = -1;
+	}
+	else if (direction == 'S')
+	{
+		dir.x = 0;
+		dir.y = 1;
+	}
+	else if (direction == 'E')
+	{
+		dir.x = 1;
+		dir.y = 0;
+	}
+	else if (direction == 'W')
+	{
+		dir.x = -1;
+		dir.y = 0;
+	}
+	return (dir);
 }
 
-t_point find_player_position(t_scene *scene)
+t_point	find_player_position(t_scene *scene)
 {
-    t_point position;
-    int i;
-    int j;
+	t_point	position;
+	t_point	var;
 
-    position = (t_point){-1, -1};
-    i = 0;
-    j = 0;
-    while (scene->map->map_2d[i])
-    {
-        j = 0;
-        while (scene->map->map_2d[i][j])
-        {
-            if (scene->map->map_2d[i][j] == 'N' || 
-                scene->map->map_2d[i][j] == 'S' || 
-                scene->map->map_2d[i][j] == 'E' || 
-                scene->map->map_2d[i][j] == 'W')
-            {
-                position.x = j + 0.5;
-                position.y = i + 0.5;
-                return position;
-            }
-            j++;
-        }
-        i++;
-    }
-    return (position);
+	position = (t_point){-1, -1};
+	var = (t_point){0, 0};
+	while (scene->map->map_2d[(int)var.y])
+	{
+		var.x = 0;
+		while (scene->map->map_2d[(int)var.y][(int)var.x])
+		{
+			if (scene->map->map_2d[(int)var.y][(int)var.x] == 'N'
+				|| scene->map->map_2d[(int)var.y][(int)var.x] == 'S'
+				|| scene->map->map_2d[(int)var.y][(int)var.x] == 'E'
+				|| scene->map->map_2d[(int)var.y][(int)var.x] == 'W')
+			{
+				position.x = var.x + 0.5;
+				position.y = var.y + 0.5;
+				return (position);
+			}
+			var.x++;
+		}
+		var.y++;
+	}
+	return (position);
 }
 
-t_point find_plane_direction(t_point direction)
+t_point	find_plane_direction(t_point direction)
 {
-    t_point plane_vector;
-    
-    plane_vector.x = 0;
-    plane_vector.y = 0;
-    if (direction.x == 0 && direction.y == -1)
-    {
-        plane_vector.x = 0.66;
-        plane_vector.y = 0;
-    }
-    else if (direction.x == 0 && direction.y == 1)
-    {
-        plane_vector.x = -0.66;
-        plane_vector.y = 0;
-    }
-    else if (direction.x == 1 && direction.y == 0)
-    {
-        plane_vector.x = 0;
-        plane_vector.y = 0.66;
-    }
-    else if (direction.x == -1 && direction.y == 0)
-    {
-        plane_vector.x = 0;
-        plane_vector.y = -0.66;
-    }
-    return plane_vector;
+	t_point	plane_vector;
+
+	plane_vector.x = 0;
+	plane_vector.y = 0;
+	if (direction.x == 0 && direction.y == -1)
+	{
+		plane_vector.x = 0.66;
+		plane_vector.y = 0;
+	}
+	else if (direction.x == 0 && direction.y == 1)
+	{
+		plane_vector.x = -0.66;
+		plane_vector.y = 0;
+	}
+	else if (direction.x == 1 && direction.y == 0)
+	{
+		plane_vector.x = 0;
+		plane_vector.y = 0.66;
+	}
+	else if (direction.x == -1 && direction.y == 0)
+	{
+		plane_vector.x = 0;
+		plane_vector.y = -0.66;
+	}
+	return (plane_vector);
 }
 
-void game_setup(t_game *game)
+void	game_setup(t_game *game)
 {
-    int i;
+	int	i;
+	int	j;
+	int	temp;
 
-    i = 0;
-    game->player.position = find_player_position(game->scene);
-    game->player.direction = find_player_direction(game->scene->map->map_2d[(int)game->player.position.y][(int)game->player.position.x]);
-    game->plane_vector = find_plane_direction(game->player.direction);
-    while (i < NUM_TEXTURES)
-    {
-        game->texture_buffer[i] = NULL;
-        i++;
-    }
-    game->pixels_map = NULL;
+	i = 0;
+	temp = (int)game->player.position.x;
+	j = game->scene->map->map_2d[(int)game->player.position.y][temp];
+	game->player.position = find_player_position(game->scene);
+	game->player.direction = find_player_direction(j);
+	game->plane_vector = find_plane_direction(game->player.direction);
+	while (i < NUM_TEXTURES)
+	{
+		game->texture_buffer[i] = NULL;
+		i++;
+	}
+	game->pixels_map = NULL;
 }
